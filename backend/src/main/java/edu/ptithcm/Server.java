521f259cbc71aed643ef90447c2225a9ddfa58fe
dvquan <dinhvietquan11a1@gmail.com>
2025-10-27 15:18:07 +0700

    them

--- backend/src/main/java/edu/ptithcm/Server.java
+++ backend/src/main/java/edu/ptithcm/Server.java
@@ -4,19 +4,32 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 import edu.ptithcm.configs.Config;
+import edu.ptithcm.model.repository.EmployeeRepo;
 import edu.ptithcm.protocols.DTTP;
 import edu.ptithcm.routes.RouteManager;
+import edu.ptithcm.services.EmployeeService;
 
 public class Server {
     private static final int port = Config.AppConfig.SERVER_PORT;
     public static void main(String[] args) {
+        if(!EmployeeRepo.checkEmployeeExists("admin")) {
+            EmployeeService.createEmployee(
+                "admin",             
+                "admin123",          
+                "Administrator",     
+                "0000000000",        
+                "ADMIN"              
+            );
+            System.out.println("Admin account created with username: admin, password: admin123");
+        }
+
         try (ServerSocket serverSocket = new ServerSocket(Server.port)) {
             System.out.println("[SERVER] is running in " + port + "....");
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("[SERVER] Client mới: " + clientSocket.getInetAddress());
                 DTTP server = new DTTP(clientSocket);
-                //route
+            
                 RouteManager routeManager = new RouteManager(server);
                 routeManager.LoginRoute();   
                 server.listen();
